9e2415ef816012f00bc2afcd042370829c60ce17
Raihanulislam78s <103953723+Raihanulislam78s@users.noreply.github.com>
2022-04-18 09:47:04 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+var config = {
+    texts: [
+        "Today is...",      
+        "as beautiful as other days",  
+        "but you realize",
+        "another year has gone",
+        "in a blink of the eyes",
+        "This I actually made",
+        "as a birthday gift to you 😆",
+        "I just want to say",
+        "thanks for my life change",
+        "Love you",
+        
+
+    ],
+
+    imgs: {
+        "心爱的小可爱": "./imgs/sharna.jpg",
+    },
+    desc: {
+        turn_on: "🏮 TURN ON LIGHTS 🏮",
+        play: "🎻 PLAY MUSIC 🎻",
+        bannar_coming: "🎊 LET'S DECORATE 🎊",
+        balloons_flying: "🎈 FLY WITH BALLOONS 🎈",
+        cake_fadein: "🎂 MOST DELICIOUS CAKE EVER 🎂",
+        light_candle: "🕯 LIGHT CANDLE 🕯",
+        wish_message: "🎉 HAPPY BIRTHDAY 🎉",
+        story: "✨ MESSAGE FOR YOU ✨",
+    }
+};
